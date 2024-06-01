6f6c6a7efa68322e66bd24d50f31e3ed1fe90c03
Emilien <emilien.marotta@gmail.com>
2024-06-01 12:48:09 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const angle1 = document.getElementById("angle1");
+    const angle2 = document.getElementById("angle2");
+    
+    setTimeout( () => {
+        
+        angle1.style.transform = "translate(0, 0)";
+        angle2.style.transform = "translate(0, 0)";
+        
+    }, 500);
+
+});
